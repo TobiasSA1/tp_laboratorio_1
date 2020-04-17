5e8168e993442fe24646e02f98ad3eda75895011
TobiasSA1 <60607739+TobiasSA1@users.noreply.github.com>
2020-04-17 08:30:13 +0200

    Add files via upload

---
+++ TP 1/LlamadoFunciones.h
@@ -0,0 +1,8 @@
+#include "Funciones.c"
+
+int pedirNumero(int A);
+int OperacionSuma (int A,int B);
+int OperacionResta (int A,int B);
+int OperacionMultiplicacion (int A,int B);
+int OperacionDivision (int A,int B);
+int OperacionFactoriales (int A);
